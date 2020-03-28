b57fc20e6c75977d75422b3a1d6d81d3fdf69a82
Joe Gallego <josephjgallego@gmail.com>
2020-03-28 16:00:12 -0700

    adding notes

--- javascript_notes.js
+++ javascript_notes.js
@@ -1,3 +1,6 @@
+// Java is strictly typed
+// javascript is dynamically typed 
+
 // variable declaration
 let x;
 
@@ -19,11 +22,35 @@ x = 0;
 // variable types 
 
 // type undefined and null
-let undef;
-let itsNull = null;
+let undef; // when variable declared but not assigned
+let itsNull = null; // not holding any value, empty 
 
 // Boolean
-let isGroup = false;
+let isGroup = false; // true or false, yes or no, binary 0 or 1
 
 // number type 
+let number = 10; // can be used for mathematical equations 
 
+// string type
+let string = "string"; // can use double " or single '
+
+// array type: list of values 
+let emptyarr = [];
+
+let puppies = ["Joe", "Justin", "Alex"]
+let ages = [33, 31, 32, 43]
+
+// arrays can contain other types, not limited to 1 type (like Java)
+// examples of mixed types
+
+let arr = [33, 31, 32,"Joe", "Diana", [12, "20"], {"key1": "val1"}, 40]
+
+// example
+let people = ["me","myself","I"];
+console.log(colors[0]); // will print "me"
+
+// console.log() similar to "sout" in Java 
+
+// adding items to an array 
+people.push("you") // pushes to end of array
+console.log(people) // would print ["me","myself","I","you"]
