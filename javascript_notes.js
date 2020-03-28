60ffa2d30ed0a2a559afa8922eba7a3f7b4b4665
Joe Gallego <josephjgallego@gmail.com>
2020-03-28 15:04:40 -0700

    adding notes

---
+++ javascript_notes.js
@@ -0,0 +1,29 @@
+// variable declaration
+let x;
+
+// three types of variable declaration 
+const constantVariable;
+// const: immutable, cannot be changed once set, need to
+// be declared and assigned at the same time  
+let mutableVariable;
+// let: mutable, can be changed 
+
+// const and let created in 2015, var used to be
+// only way of declaring variables 
+
+var oldDeclaration;
+
+// variable assignment
+x = 0; 
+
+// variable types 
+
+// type undefined and null
+let undef;
+let itsNull = null;
+
+// Boolean
+let isGroup = false;
+
+// number type 
+
