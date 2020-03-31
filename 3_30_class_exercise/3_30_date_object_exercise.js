841421a8b93440c9d452b25e313125bd58fb645a
Joe Gallego <josephjgallego@gmail.com>
2020-03-30 23:46:16 -0700

    adding class exercise

--- 3_30_class_exercise/3_30_date_object_exercise.js
+++ 3_30_class_exercise/3_30_date_object_exercise.js
@@ -1,9 +1,13 @@
 // create function to display analog clock 
 
+let hours, minutes, seconds;
+
 function analogClock(){
     
+    // create new Date object and assign it to variable called "time"
     let clock = new Date();
 
+    // access the "get" functions on Date object with dot accessor
     let hours = clock.getHours();
     let minutes = clock.getMinutes();
     let seconds = clock.getSeconds();
