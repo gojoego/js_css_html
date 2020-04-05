595bc261b270c4f9b8a5af6f0114297135ee57d6
Joe Gallego <josephjgallego@gmail.com>
2020-04-04 22:54:53 -0700

    updating

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let display = document.getElementById('display-screen')
+
+let one = document.getElementById('one');
+one.addEventListeneraddEventListener("click", displayOne())
+function displayOne (){
+    if ()
+  display.innerHTML = "1";
+}
+
+
+figu
